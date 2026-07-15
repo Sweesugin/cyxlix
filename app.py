a22cb7f26ab2465dd39cf41ed2f9f1a792bbed26
unknown <icykid33.99@gmail.com>
2026-07-15 16:14:59 +0530

    Configure dynamic PORT binding for cloud deployment support

--- app.py
+++ app.py
@@ -1222,4 +1222,6 @@ def internal_server_error(e):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
